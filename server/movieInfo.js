901ac7506176bc98b405bdfc0355e63b67ab20e1
Pujap84 <81335331+Pujap84@users.noreply.github.com>
2021-09-29 23:14:39 +1000

    backend get route working for both providers

--- server/movieInfo.js
+++ server/movieInfo.js
@@ -7,15 +7,90 @@ const options = {
     headers: { "x-api-key": api_key },
 };
 
-function getFilmworldMovieInfo() {
-    axios
-        .get(
-            `https://challenge.lexicondigital.com.au/api/v2/filmworld/movies`,
-            options
-        )
+function getMovieInfo() {
+    let URLs = [
+        "https://challenge.lexicondigital.com.au/api/v2/cinemaworld/movies",
+        "https://challenge.lexicondigital.com.au/api/v2/filmworld/movies",
+    ];
+    return Promise.all(URLs.map(fetchData));
+}
+
+function fetchData(URL) {
+    return axios
+        .get(URL, options)
         .then((response) => {
-            const movieURL = response.data;
-            console.log(movieURL);
+            return response.data;
+        })
+        .catch((error) => {
+            console.log(error);
         });
 }
-getFilmworldMovieInfo();
+
+getMovieInfo()
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+// let URLs = [
+//     "https://challenge.lexicondigital.com.au/api/v2/cinemaworld/movies",
+//     "https://challenge.lexicondigital.com.au/api/v2/filmworld/movies",
+// ];
+
+// function getAllData(URLs) {
+//     return Promise.all(URLs.map(fetchData));
+// }
+
+// function fetchData(URL) {
+//     return axios
+//         .get(URL, options)
+//         .then((response) => {
+//             return response.data;
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+// }
+
+// getAllData(URLs)
+//     .then((resp) => {
+//         console.log(resp);
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     });
+
+// let URL1 = "https://challenge.lexicondigital.com.au/api/v2/cinemaworld/movies";
+// let URL2 = "https://challenge.lexicondigital.com.au/api/v2/filmworld/movies";
+
+// const promise1 = axios.get(URL1);
+// const promise2 = axios.get(URL2);
+
+// Promise.all([promise1, promise2]).then((values) => {
+//     return values;
+// });
+
+// function getCinemaworldMovieInfo() {
+//     let promise1 = axios.get(
+//         `https://challenge.lexicondigital.com.au/api/v2/cinemaworld/movies`,
+//         options
+//     );
+//     let promise2 = axios.get(
+//         `https://challenge.lexicondigital.com.au/api/v2/filmworld/movies`,
+//         options
+//     );
+//     Promise.all([promise1, promise2])
+//         .then((values) => {
+//             return values;
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+// }
+
+// getCinemaworldMovieInfo();
+// module.exports = getCinemaworldMovieInfo;
+// getAllData();
+module.exports = getMovieInfo;
--- server/server.js
+++ server/server.js
@@ -5,10 +5,38 @@ const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
 
+let getMovieInfo = require("./movieInfo.js");
+
 // middleware to read JSON body data
 app.use(express.json());
 
 // Routes
-// app.get("/burgerLayers", (req, res) => {
-//     res.json({ burgerLayers });
+
+app.get("/api/message", async (req, res) => {
+    const response = await getMovieInfo();
+    console.log(response);
+    res.json(response);
+});
+
+// app.get("/api/test", (req, res) => {
+//     getCinemaworldMovieInfo();
+//     res.json({ test: "test" });
+// });
+
+// app.get("/api", (req, res) => {
+//     res.json({});
+// });
+
+// app.get("/api/message", async (req, res) => {
+//     console.log(req.body.message);
+//     const message = req.body.message;
+//     getCinemaworldMovieInfo(message).then((response) =>
+//         res.json({ message: response })
+//     );
+//     res.json({ test: req.body.message });
+// });
+
+// app.get("/api/message", async (req, res) => {
+//     const response = await getFilmworldMovieInfo(req.query.message);
+//     res.json(response);
 // });
